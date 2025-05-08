0f98276ec6020ac4f7e7bb5dfb20a313f9ef8502
EmmanuelAlvarezGiles <20243rd056@utez.edu.mx>
2025-05-08 11:45:37 -0600

    Guarda el nombre

---
+++ Cadenas.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Cadenas{
+    public static void main(String[] args){
+        Scanner almacenar = new Scanner(System.in);
+        System.out.println("Escribe tu nombre: ");
+        String nombre = almacenar.nextLine();
+
+        System.out.println("Tu nombre es: " + nombre);
+    }
+}
